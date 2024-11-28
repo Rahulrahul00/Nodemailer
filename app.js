702e40980d3ddd41b7be1393e6240417501d0bb9
Rahul c shaji <rahulcshaji007@gmail.com>
2024-11-28 19:42:05 +0530

    Created sample contact form

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemon = require('nodemon');
+const nodemailer = require('nodemailer');
+
+const app = express();
+const port = 3000;
+
+
+
+
+
+app.get()
+
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+})
