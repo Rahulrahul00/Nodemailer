40c72e318644b1a592acb64240a901b3f4492018
Rahul c shaji <rahulcshaji007@gmail.com>
2024-11-28 23:34:19 +0530

    successfully sent mail by using nodemailer

--- app.js
+++ app.js
@@ -1,16 +1,59 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const nodemon = require('nodemon');
 const nodemailer = require('nodemailer');
+const { consumers } = require('nodemailer/lib/xoauth2');
 
 const app = express();
-const port = 3000;
+const port = 4000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/",(req, res)=>{
+    res.sendFile(__dirname + "/index.html");
+    console.log(__dirname);
+
+})
+
+app.post("/", (req, res)=>{
+    const msg = req.body.message;
+    const name = req.body.nameofperson;
+ const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth: {
+            user: "rahulcshaji007@gmail.com",
+            pass: "uiuoapyprkpcdjkj",
+          },
+ }) 
+
+ const mailOption = {
+    from : 'rahulcshaji007@gmail.com',
+    to : req.body.username,
+    cc :'rahulcshaji007@gmail.com',
+    subject: `Thanks for giving feedback ${name}`,
+    text: `Thanks for your message you have sent to us ${msg}`
+ }
+
+ transporter.sendMail(mailOption, (error, info)=>{
+    if(error){
+        console.log(error);
+    }else{
+        res.redirect('/');
+        console.log(`email sent ${info.response}`)
+    }
+
+ })
+    
+   
+});
 
 
 
 
 
-app.get()
+
 
 
 
